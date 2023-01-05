a3c5cbdf915a2658610f551a123e5a8e190149cb
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-05 17:57:30 +0200

    add component CharInfo

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -34,25 +34,23 @@ class CharList extends Component {
         })
     }
 
-    // Этот метод создан для оптимизации,
-    // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
-        const items =  arr.map((item) => {
-            let imgStyle = {'objectFit' : 'cover'};
+        const items = arr.map((item) => {
+            let imgStyle = {'objectFit': 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-                imgStyle = {'objectFit' : 'unset'};
+                imgStyle = {'objectFit': 'unset'};
             }
 
             return (
                 <li
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={()=>this.props.onCharSelected(item.id)}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
             )
         });
-        // А эта конструкция вынесена для центровки спиннера/ошибки
         return (
             <ul className="char__grid">
                 {items}
