837eacdc589f56e5bd06f927eba5fb94953b481c
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-26 17:38:34 +0200

    add Page404

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import {ComicsPage, MainPage} from '../pages';
+import {ComicsPage, MainPage, Page404} from '../pages';
 
 const App = () => {
 
@@ -13,6 +13,7 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<MainPage/>}/>
                         <Route path="/comics" element={<ComicsPage/>}/>
+                        <Route path="*" element={<Page404/>}/>
                     </Routes>
                 </main>
             </div>
